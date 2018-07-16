0d1c912f74e960ed3e45d4f2af3177c9d1916b04
Wells <chawkeye@gmail.com>
2018-07-16 20:37:54 +0800

    set resources lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :create] do
+    get :generate_km
+  end
+
+  root "lists#index"
 end
