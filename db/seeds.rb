54c9feb0757912674f873261e5304a7cb6380ddb
Wells <chawkeye@gmail.com>
2018-07-17 22:40:52 +0800

    使用前須先登入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "admin@24tms.com.tw", password: "tmsadmin")
