6322937d2f12b6514da369620d2515ecbf1a283f
Wells <chawkeye@gmail.com>
2018-07-18 22:04:06 +0800

    增加post model 紀錄郵遞區號表

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180717142607) do
+ActiveRecord::Schema.define(version: 20180718140239) do
 
   create_table "lists", force: :cascade do |t|
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: ""
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tmsrecords", force: :cascade do |t|
