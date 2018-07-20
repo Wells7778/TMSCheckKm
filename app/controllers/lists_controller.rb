53b059ac56345d224fa7b7239849433d806336bb
Wells <chawkeye@gmail.com>
2018-07-20 21:46:17 +0800

    調整匯入檔案格式

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -26,23 +26,23 @@ class ListsController < ApplicationController
       redirect_to root_path
     else
       xlsx = Roo::Excelx.new(@list.location.path)
+      sheet = xlsx.sheet('服務案件明細表')
       url = "https://maps.googleapis.com/maps/api/directions/json"
       map_config = Rails.application.config_for(:map)
       key = map_config["secret"]
       # 讀入每一行資料
-      xlsx.each(number: "報修單號", post_code: "郵遞區號", dest: "目的地地址", driver_km: "公里數") do |hash|
-        if hash[:driver_km].to_i > 10 && hash[:number] != "報修單號"
-          dest_code = hash[:dest][0..5]
-          c_dest = hash[:dest][6..-1].split("(").first.split("/").map! {|x| x.length >= 5 ? x : "" }.join("").split("ZZZ").map! {|x| x.length >= 5 ? x : "" }.join("").split(" ").map! {|x| x.length >= 5 ? x : "" }.join("")
-          c_dest = dest_code + c_dest
-          origin = Post.find_by(code: hash[:post_code]).name
+      sheet.each(number: "報修單號", origin_area: "服務區域", dest: "拖吊地點", dest_area: "目的區域", driver_km: "拖吊里程") do |hash|
+        if hash[:driver_km].to_i > 10 && hash[:origin_area][0..1] != "國道" && hash[:number] != "報修單號"
+          c_dest = hash[:dest].split("(").first.split("/").map! {|x| x.length >= 5 ? x : "" }.join("").split("ZZZ").map! {|x| x.length >= 5 ? x : "" }.join("").split(" ").map! {|x| x.length >= 5 ? x : "" }.join("")
+          c_dest = hash[:dest_area] + c_dest
+          origin = hash[:origin_area]
           json_rep = RestClient.get url, {params: {origin: origin, destination: c_dest , language: 'zh-TW', key: key, avoid: "highways" }}
           respond = JSON.parse(json_rep)
           c_km = 0
           if respond["status"] == "OK"
             c_km = respond["routes"][0]["legs"][0]["distance"]["value"] / 1000
           end
-          @list.tmsrecords.create(number: hash[:number], post_code: hash[:post_code], dest: hash[:dest], driver_km: hash[:driver_km], c_km: c_km, diff_km: hash[:driver_km] - c_km, status: respond["status"])
+          @list.tmsrecords.create(number: hash[:number], post_code: hash[:origin_area], dest: hash[:dest], driver_km: hash[:driver_km], c_km: c_km, diff_km: hash[:driver_km] - c_km, status: respond["status"])
         end
       end
       if @list.update(status: "IMPORT")
